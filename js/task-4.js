6e36ee1a733bef84fbd7d7ac399b157bc5a43e7c
KaterynaKocheharova <kot41901@gmail.com>
2024-01-20 16:57:48 +0200

    done task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,32 @@
+// =================================================== VARIABLES =============================================
 
+const loginForm = document.querySelector(".login-form");
+
+// ======================================= HANDLE LOGIN FORM SUBMIT EVENT FUNCTION ===========================
+
+const handleLogining = (event) => {
+
+    // preventing updating of the page
+event.preventDefault();
+
+// variables
+const form = event.target;
+const email = form.elements.email.value;
+const password = form.elements.password.value;
+const valuesObject = {};
+
+// Do I need trim here? Просто без метода трім все і так працює, тобто незважаючи на те яку кількість пробілів я ввела,
+// воно все рівно не пропускає. Тобто будь-який пробіл вважається "". Якось дивно
+if(email.trim() === "" || password.trim() === "") {
+    alert("All form fields must be filled in");
+} else {
+    valuesObject.email = form.elements.email.value;
+    valuesObject.password = form.elements.password.value;
+    console.log(valuesObject);
+    form.reset();
+}
+}
+
+// ============================================ ADDING EVENT TO THE FORM =======================================
+
+loginForm.addEventListener("submit", handleLogining);
