d1af5716cbb6b52e290ab2395e46bd0b2e055c6f
KaterynaKocheharova <kot41901@gmail.com>
2024-01-21 17:22:12 +0200

    task 5 done

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,25 @@
+// ================================== GENERATE RANDOM COLOR FUNCTION ========================================
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+// ============================================== VRIABLES ==================================================
+
+const changeColorBtn = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const colorSpan = document.querySelector(".color");
+
+// ======================================= CHANGE COLORS FUNCTION ===========================================
+
+const changeColor = () => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+}
+
+// ======================================= ADDING EVEN LISTENER ============================================
+
+changeColorBtn.addEventListener("click", changeColor);
